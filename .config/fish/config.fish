385f0765a247ff9e373a68cea69ee9272380b50c
alishir <aj.shirvani@gmail.com>
2019-11-01 06:17:34 +0330

    update fish config

--- .config/fish/config.fish
+++
@@ -1,7 +0,0 @@
-source /home/ali/anaconda3/etc/fish/conf.d/conda.fish
-set fish_color_command normal
-set theme_color_scheme zenburn
-
-alias config="/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME"
-
-set PATH /home/ali/bin $PATH
