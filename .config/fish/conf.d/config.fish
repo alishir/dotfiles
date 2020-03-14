3f5ed36a59e0b2476b5e23545f3dacade6b4dd29
Ali Shirvani <aj.shirvani@gmail.com>
2020-03-14 07:29:31 +0330

    add fish config.

---
+++ .config/fish/conf.d/config.fish
@@ -0,0 +1,8 @@
+set fish_color_command normal
+set theme_color_scheme zenburn
+
+alias config="/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME"
+
+set PATH /home/ali/bin $PATH
+set PATH /home/ali/.kiex/bin/ $PATH
+set PATH /home/ali/dev-tools/node/current/bin $PATH
