182e377c420fc0d8ff3d75a13b8c7abe1d2e26b1
Ali Shirvani <aj.shirvani@gmail.com>
2021-02-11 08:08:07 +0330

    emacs rust-mode.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -14,92 +14,25 @@
       (eval-print-last-sexp)))
   (load bootstrap-file nil 'nomessage))
 
-(straight-use-package 'use-package)
-(require 'use-package)
-
-;; ===============
-;; yaml
-;; ===============
-(straight-use-package 'yaml-mode)
-
-;; ===============
-;; helm
-;; ===============
-(straight-use-package 'helm)
-(global-set-key (kbd "M-x") 'helm-M-x)
-(global-set-key (kbd "C-x C-f") 'helm-find-files)
-(helm-mode 1)
-
-;; ======================
-;; lsp-mode
-;; ======================
-(straight-use-package 'lsp-mode)
-(straight-use-package 'helm-lsp)
-(straight-use-package 'yasnippet)
-(straight-use-package 'lsp-treemacs)
-(straight-use-package 'projectile)
-(straight-use-package 'hydra)
 (straight-use-package 'flycheck)
 (straight-use-package 'company)
 (straight-use-package 'avy)
-(straight-use-package 'which-key)
-(straight-use-package 'helm-xref)
-(straight-use-package 'dap-mode)
+(straight-use-package 'use-package)
+(require 'use-package)
 
-;; set prefix for lsp-command-keymap (few alternatives - "C-l", "C-c l")
-(setq lsp-keymap-prefix "C-l")
-
-(use-package lsp-mode
-    :hook (;; replace XXX-mode with concrete major-mode(e. g. python-mode)
-            (erlang-mode . lsp)
-            ;; if you want which-key integration
-            (lsp-mode . lsp-enable-which-key-integration))
-    :commands lsp)
-
-;; optionally
-(use-package lsp-ui :commands lsp-ui-mode)
-;; if you are helm user
-(use-package helm-lsp :commands helm-lsp-workspace-symbol)
-;; if you are ivy user
-(use-package lsp-ivy :commands lsp-ivy-workspace-symbol)
-(use-package lsp-treemacs :commands lsp-treemacs-errors-list)
-
-;; optionally if you want to use debugger
-(use-package dap-mode)
-;; (use-package dap-LANGUAGE) to load the dap adapter for your language
-
-;; optional if you want which-key integration
-(use-package which-key
-    :config
-    (which-key-mode))
+;; ==============
+;; PATH
+;; ==============
+(add-to-list 'exec-path "/home/ali/bin/")
+(setenv "PATH" (concat (getenv "PATH") ":/home/ali/.kerl/23.2.1/bin"))
+(setenv "PATH" (concat (getenv "PATH") ":/home/ali/.cargo/bin"))
 
 ;; ======================
-;; C++
+;; RUST
 ;; ======================
-;; sample `helm' configuration use https://github.com/emacs-helm/helm/ for details
-(helm-mode)
-(require 'helm-xref)
-(define-key global-map [remap find-file] #'helm-find-files)
-(define-key global-map [remap execute-extended-command] #'helm-M-x)
-(define-key global-map [remap switch-to-buffer] #'helm-mini)
-
-(which-key-mode)
-(add-hook 'c-mode-hook 'lsp)
-(add-hook 'c++-mode-hook 'lsp)
-
-(setq gc-cons-threshold (* 100 1024 1024)
-      read-process-output-max (* 1024 1024)
-      treemacs-space-between-root-nodes nil
-      company-idle-delay 0.0
-      company-minimum-prefix-length 1
-      lsp-idle-delay 0.1 ;; clangd is fast
-      ;; be more ide-ish
-      lsp-headerline-breadcrumb-enable t)
-
-(with-eval-after-load 'lsp-mode
-  (add-hook 'lsp-mode-hook #'lsp-enable-which-key-integration)
-  (require 'dap-cpptools)
-  (yas-global-mode))
+(use-package rust-mode
+  :straight t
+  :ensure t)
 
 ;; ======================
 ;; Erlang
@@ -134,6 +67,93 @@
 	 ("\\Emakefile" . erlang-mode)))
 
 
+;; ===============
+;; yaml
+;; ===============
+(straight-use-package 'yaml-mode)
+
+;; ===============
+;; helm
+;; ===============
+(straight-use-package 'helm)
+(global-set-key (kbd "M-x") 'helm-M-x)
+(global-set-key (kbd "C-x C-f") 'helm-find-files)
+(helm-mode 1)
+
+;; ======================
+;; lsp-mode
+;; ======================
+(straight-use-package 'lsp-mode)
+(straight-use-package 'helm-lsp)
+(straight-use-package 'yasnippet)
+(straight-use-package 'lsp-treemacs)
+(straight-use-package 'projectile)
+(straight-use-package 'hydra)
+(straight-use-package 'which-key)
+(straight-use-package 'helm-xref)
+(straight-use-package 'dap-mode)
+
+;; set prefix for lsp-command-keymap (few alternatives - "C-l", "C-c l")
+(setq lsp-keymap-prefix "C-l")
+
+(use-package lsp-mode
+  :hook (;; replace XXX-mode with concrete major-mode(e. g. python-mode)
+         (erlang-mode . lsp)
+	 (rust-mode . lsp)
+         ;; if you want which-key integration
+         (lsp-mode . lsp-enable-which-key-integration))
+  :commands lsp)
+
+;; optionally
+(use-package lsp-ui :commands lsp-ui-mode)
+;; if you are helm user
+(use-package helm-lsp :commands helm-lsp-workspace-symbol)
+;; if you are ivy user
+(use-package lsp-ivy :commands lsp-ivy-workspace-symbol)
+(use-package lsp-treemacs :commands lsp-treemacs-errors-list)
+
+;; optionally if you want to use debugger
+(use-package dap-mode)
+;; (use-package dap-LANGUAGE) to load the dap adapter for your language
+
+;; optional if you want which-key integration
+(use-package which-key
+    :config
+    (which-key-mode))
+
+(setq lsp-prefer-capf t)
+(setq lsp-completion-provider :capf)
+(setq lsp-completion-enable t)
+
+;; ======================
+;; C++
+;; ======================
+;; sample `helm' configuration use https://github.com/emacs-helm/helm/ for details
+(helm-mode)
+(require 'helm-xref)
+(define-key global-map [remap find-file] #'helm-find-files)
+(define-key global-map [remap execute-extended-command] #'helm-M-x)
+(define-key global-map [remap switch-to-buffer] #'helm-mini)
+
+(which-key-mode)
+(add-hook 'c-mode-hook 'lsp)
+(add-hook 'c++-mode-hook 'lsp)
+
+(setq gc-cons-threshold (* 100 1024 1024)
+      read-process-output-max (* 1024 1024)
+      treemacs-space-between-root-nodes nil
+      company-idle-delay 0.0
+      company-minimum-prefix-length 1
+      lsp-idle-delay 0.1 ;; clangd is fast
+      ;; be more ide-ish
+      lsp-headerline-breadcrumb-enable t)
+
+(with-eval-after-load 'lsp-mode
+  (add-hook 'lsp-mode-hook #'lsp-enable-which-key-integration)
+  (require 'dap-cpptools)
+  (yas-global-mode))
+
+
 (use-package flycheck
   :straight t
   :ensure t
@@ -164,7 +184,6 @@
 ;; orgmode
 ;; ======================
 (straight-use-package 'org)
-
 (defun set-bidi-env ()
   "interactive"
   (setq bidi-paragraph-direction 'nil))
@@ -175,7 +194,6 @@
 (global-set-key (kbd "C-c a") 'org-agenda)
 (global-set-key (kbd "C-c c") 'org-capture)
 
-;;(require 'org-tempo)
 
 ;; ====================
 ;; general keybindings
@@ -226,11 +244,9 @@
                     :width 'normal)
 
 ;; Window appearance
-;;(when window-system 
 (set-fontset-font "fontset-default" '(#x0600 . #x06FF)  "Vazir")
 (set-fontset-font "fontset-default" 'unicode  "Vazir")
 ;;  (set-face-attribute 'default nil :height 120)
-;;  )
 
 (tool-bar-mode -1)
 (menu-bar-mode -1)
@@ -239,5 +255,3 @@
 ;; https://emacs.stackexchange.com/questions/3458/how-to-switch-between-windows-quickly
 (windmove-default-keybindings)
 
-(add-to-list 'exec-path "/home/ali/bin/")
-(setenv "PATH" (concat (getenv "PATH") ":/home/ali/.kerl/23.2.1/bin"))
